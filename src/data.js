7bad0fb9e08a8fe018801f71fef783281f1508af
io62s <ufo62s@gmail.com>
2019-03-20 14:20:40 +0100

    phone data updated

--- src/data.js
+++ src/data.js
@@ -88,7 +88,7 @@ export const storeProducts = [
     title: "Oneplus 6 amber-red",
     img: "img/product-8.png",
     price: 20,
-    company: "apple",
+    company: "OnePlus",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
